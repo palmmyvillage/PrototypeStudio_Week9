1192342913f107719f21317480b430faa0c2081c
palmmyvillage <43654403+palmmyvillage@users.noreply.github.com>
2019-11-06 08:04:59 -0500

    Update ResetButton

---
+++ PrototypeStudio_Week9/Assets/Scripts/ResetButton.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResetButton : MonoBehaviour
+{
+    public static ResetButton instance;
+    public KeyCode resetSceneButton, resetGameButton, quitGameButton, nextSceneButton;
+    public bool nextScene_enable;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(resetSceneButton))
+            ResetScene();
+        if (Input.GetKeyDown(resetGameButton))
+            ResetGame();
+        if (Input.GetKeyDown(quitGameButton))
+            QuitGame();
+
+        if (Input.GetKeyDown(nextSceneButton))
+        {
+            if (nextScene_enable == true)
+                GoNextScene();
+        }
+        
+    }
+
+    void ResetScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    void ResetGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    void GoNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
